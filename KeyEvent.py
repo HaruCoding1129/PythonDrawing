c91580d120a0826d93cf4eced3f0ee44afa2e021
HaruCoding1129 <50877389+HaruCoding1129@users.noreply.github.com>
2019-09-27 12:25:56 +0900

    Update KeyEvent.py

--- KeyEvent.py
+++ KeyEvent.py
@@ -1,54 +1,80 @@
 import turtle as t
 
-# 터틀의 기본방향은 오른쪽으로부터 시작합니다.
-# 따라서 방향을 90도, 180도 270도를 틀면 윗쪽, 왼쪽, 아랫쪽 방향이 됩니다. 
 
-# 오른쪽
+# ========== 방향키 ==========
 def right():
     t.setheading(0)
     t.forward(10)
 
-# 위
 def up():
     t.setheading(90)
     t.forward(10)
 
-# 왼쪽
 def left():
     t.setheading(180)
     t.forward(10)
 
-# 아래
 def down():
     t.setheading(270)
     t.forward(10)
+    
 
-# 1번키 : 파란색
+# ========== 색상변경 ==========
 def blue_1():
     t.pencolor("blue")
 
-# 2번키 : 빨강색
 def red_2():
     t.pencolor("red")
 
-# 3번키 : 검정색
 def black_3():
     t.pencolor("black")
-    
-# 키 등록 : ﻿onkey(fun, key)﻿
-# fun 에는 위에서 만든 함수이름을 써주면 됩니다.
-# key 에는 설정하고 싶은 키를 입력하면 됩니다.
 
+    
+# ========== 도형그리기 ==========
+def square_4():
+  for i in range(0,4):
+    t.forward(200) 
+    t.left(90) 
+
+def circle_5():
+  t.circle(100)
+
+def star_6():
+  for i in range(0,5):
+     t.forward(200)
+     t.right(144)
+
+    
+# ========== 펜 Up & Down ==========
+def square_q():
+  t.penup()
+
+def circle_w():
+  t.down()
+
+
+# ========== 키 등록 ==========
+
+#방향키
 t.onkey(up, "Up")
 t.onkey(down, "Down")
 t.onkey(right, "Right")
 t.onkey(left, "Left")
+
+#색상변경
 t.onkey(blue_1, "1")
 t.onkey(red_2, "2")
 t.onkey(black_3, "3")
 
-# onkey로 등록된 키를 누르면 listen()에게 값이 전달이 됩니다.
-# listen()을 생략하면 키보드를 아무리 눌려도 어떤 반응도 나타나지 않습니다.
-# 한번 생략 해 보세요!
+#도형그리기
+t.onkey(square_4, "4")
+t.onkey(circle_5, "5")
+t.onkey(star_6, "6")
 
+#펜 Up & Down
+t.onkey(square_4, "q")
+t.onkey(circle_5, "w")
+
+
+# ========== listen() 잊지 마세요! ==========
 t.listen()
